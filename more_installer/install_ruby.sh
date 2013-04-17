7fd38f9da38baccf8684bc2caacf83579886195e
Kazuhiro Homma <kazu.homma@gmail.com>
2013-04-17 00:20:14 +0000

    rubyのインストーラーを追加

--- more_installer/install_ruby.sh
+++ more_installer/install_ruby.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-which rbenv > /dev/null 2>&1
-if [ $? -eq 0 ] ; then
+if [ -f ~/.rbenv ] ; then
     echo exists rbenv
 else
     git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 fi
 
-which rbenv install -l > /dev/null 2>&1
-if [ $? -eq 0 ] ; then
+if [ -f ~/.rbenv/plugins/ruby-build ] ; then
     echo exists ruby-build
 else
     git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
