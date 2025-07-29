c131b528036548dd22536436cbb5f014c0473023
Kazuhiro Homma <kazu.homma@gmail.com>
2025-07-29 10:19:23 +0900

    update

--- 04_ln.sh
+++ 04_ln.sh
@@ -2,3 +2,7 @@ ln -sfv ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sfv ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sfv ~/dotfiles/.tmux.conf ~/.tmux.conf
 
+# Ghostty configuration
+mkdir -p ~/.config/ghostty
+ln -sfv ~/dotfiles/ghostty/config ~/.config/ghostty/config
+
